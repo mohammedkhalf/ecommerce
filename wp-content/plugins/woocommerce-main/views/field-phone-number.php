c14c62e1468ce3e1f730c99183a93022240cea5f
khalf <mohamed.khalaf@cowpay.me>
2023-12-18 16:08:23 +0200

    fix phone number index

--- wp-content/plugins/woocommerce-main/views/field-phone-number.php
+++ wp-content/plugins/woocommerce-main/views/field-phone-number.php
@@ -1,2 +1,2 @@
 <input type='text' name='cowpay_settings[YOUR_PHONE_NUMBER]' value='<?php echo esc_html($options['YOUR_PHONE_NUMBER']) ?>'>
-<p><?php esc_html_e('This is the PHONE NUMBER provided by Cowpay when you signed up for an account.', 'cowpay') ?></p>
+<p><?php esc_html_e('This phone Number must be the same when you signed up for Cowpay account.', 'cowpay') ?></p>
