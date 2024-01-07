0e17c5df27a307a6726f0f120b647eeae2203242
khalf <mohamed.khalaf@cowpay.me>
2024-01-07 21:21:06 +0200

    process credit card

--- wp-content/plugins/woocommerce-main/public/js/iframe-popup.js
+++ wp-content/plugins/woocommerce-main/public/js/iframe-popup.js
@@ -1,16 +1,14 @@
 jQuery(document).ready(function ($) {
 	console.log('Load Redirect Url')
-	function redirectSuccess(delay) {
-		setTimeout(function() {
 			var timesRefreshed = 0;
 			var redirectUrl = cowpay_data.return_url;
 			$("iframe").load(function(){
 				timesRefreshed++; 
 				if(timesRefreshed == 2){
-					window.location.href = redirectUrl;
+					setTimeout(function() {
+						window.location.href = redirectUrl;
+					}, 9000);
 				}
 			});
-		}, delay);
-	}
-	redirectSuccess(12000);   
+	
 });
