80c80308bd4712767253c82a97558b6558b3f4a9
khalf <mohamed.khalaf@cowpay.me>
2024-02-07 21:14:15 +0200

    process credit card

--- wp-content/plugins/woocommerce-main/views/credit-card-form.php
+++ wp-content/plugins/woocommerce-main/views/credit-card-form.php
@@ -1,10 +1,2 @@
-<html lang="en">
-    <body>
-        <!-- Required div to display cowpay checkout button inside, 
-             with any id to be passed in mount() method. ! -->
-             <div id="cowpay-checkout"></div>
+<div id="cowpay-checkout"></div>
 
-        <!-- Loading Cowpay.js SDK -->
-        <script src="cowpay.js"></script>
-    </body>
-</html>
