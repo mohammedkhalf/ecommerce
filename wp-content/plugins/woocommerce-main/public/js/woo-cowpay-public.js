4099c9a188cfaac527b4cf15ff717c63a3b24303
khalf <mohamed.khalaf@cowpay.me>
2024-02-07 21:23:42 +0200

    process credit card

--- wp-content/plugins/woocommerce-main/public/js/woo-cowpay-public.js
+++ wp-content/plugins/woocommerce-main/public/js/woo-cowpay-public.js
@@ -1,6 +1,6 @@
 jQuery(window).ready(function(){
   "use strict";
-  jQuery('body').on('updated_checkout', function(){
+  jQuery('body').on('#place_order', function(){
     if(cowpay_data.intentionSecret && cowpay_data.frameCode){
       console.log("hello")
       Cowpay.checkout(cowpay_data.intentionSecret,cowpay_data.frameCode).mount("cowpay-checkout");
