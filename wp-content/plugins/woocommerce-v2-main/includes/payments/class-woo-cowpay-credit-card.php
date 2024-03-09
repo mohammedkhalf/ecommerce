0cf5acf68d28c56d149ef7ba48ccd45f78611db2
khalf <mohamed.khalaf@cowpay.me>
2024-03-09 10:56:27 +0200

    process credit

--- wp-content/plugins/woocommerce-v2-main/includes/payments/class-woo-cowpay-credit-card.php
+++ wp-content/plugins/woocommerce-v2-main/includes/payments/class-woo-cowpay-credit-card.php
@@ -275,6 +275,8 @@ class WC_Payment_Gateway_Cowpay_CC extends WC_Payment_Gateway_Cowpay
             $data = ['secret' =>$response->data->intentionSecret,'frameCode'=>$request_params['frameCode']];
             $_SESSION['creditCard'] =  $data;// array
 
+            var_dump($data);
+
 
             WC()->cart->empty_cart();
             // wait server-to-server notification
