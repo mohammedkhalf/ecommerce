e9b74cf88007f2b6df6aaeb65287a96e9a97d8ad
khalf <mohamed.khalaf@cowpay.me>
2024-03-09 07:36:57 +0200

    process credit

--- wp-content/plugins/woocommerce-v2-main/includes/class-woo-cowpay.php
+++ wp-content/plugins/woocommerce-v2-main/includes/class-woo-cowpay.php
@@ -204,7 +204,7 @@ class WooCowpay
 					<!-- Required div to display cowpay checkout button inside, with any id to be passed in mount() method. ! -->
 					<div id="cowpay-checkout"></div>
 					<!-- Loading Cowpay.js SDK -->
-					<script src="./public/js/cowpay.js"></script>
+					<script src="public/js/cowpay.js"></script>
 				</body>
 				</html>';
 			die;
