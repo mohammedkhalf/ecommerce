d950f847773dde7c65315519d98507e57268bf2b
khalf <mohamed.khalaf@cowpay.me>
2024-03-10 23:06:28 +0200

    process credit

--- wp-content/plugins/woocommerce-v2-main/views/custom-otp-page.php
+++ wp-content/plugins/woocommerce-v2-main/views/custom-otp-page.php
@@ -1,11 +1,9 @@
-<html lang="en">
-    <body>
+<div>
         <!-- Required div to display cowpay checkout button inside, with any id to be passed in mount() method. ! -->
         <div id="cowpay-checkout"></div>
         <!-- Loading Cowpay.js SDK -->
         <script src="public/js/cowpay.js"></script>
-    </body>
-</html>      
+</div>
 
 <script>
     Cowpay.checkout("<?php echo $options['secret']; ?>","<?php  echo $options['frameCode']; ?>").mount("cowpay-checkout");
